a45d305e0325ec3beae2b347b484e037e432e099
Christopher Cliff <chris@percolate.com>
2013-09-18 17:42:13 -0400

    Fixed test.

--- test/index.js
+++ test/index.js
@@ -1,17 +1,18 @@
-var should = require('should'),
-    own = require('../');
+var should = require('should')
+var own = require('../')
 
 describe('own', function(){
-    
+
     it('should create an own properties object', function(){
         own({
             foo: 'bar'
         }).should.eql({
             foo: {
                 value: 'bar',
-                enumerable: true
+                enumerable: true,
+                writable: true
             }
-        });
-    });
-    
-});
+        })
+    })
+
+})
